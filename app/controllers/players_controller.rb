6da1840daeab9b6d68ac2688576779e4cc89f327
rramonfraga <rafael_ramon_fraga@yahoo.es>
2016-10-14 22:48:58 +0200

    Add new and create in player controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,10 +1,29 @@
 class PlayersController < ApplicationController
   before_action :authenticate_user!
 
+  def new
+    @team = Team.find_by(id: params[:team_id])
+    @player = @team.players.new
+  end
+
+  def create
+    @team = Team.find_by(id: params[:team_id])
+    @player = @team.players.new player_params
+    if @player.save
+      redirect_to action: 'show', controller: 'teams', community_code: current_community.code, id: @team.id
+    else
+      render(:new)
+    end
+  end
+
   def destroy
     player = Player.find_by(id: params[:id])
     player.destroy
     redirect_to action: 'show', controller: 'teams', team_id: params["team_id"]
   end
 
+  private
+  def player_params
+    params.require(:player).permit(:dorsal, :name, :player_template_id)
+  end
 end
