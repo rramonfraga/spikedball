bc38a952601959c4b8ce6ecccee867ce4c0a5e08
rramonfraga <rafael_ramon_fraga@yahoo.es>
2016-10-23 17:33:05 +0200

    Save name for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,9 +12,9 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-      user_params.permit(:name, :email)
-    end
+    added_attrs = [:name, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 
   def current_community
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
 
   has_many :user_communities
   has_many :communities, through: :user_communities
-
   has_many :teams
 
   def free_teams
