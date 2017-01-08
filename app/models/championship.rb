4d89c1deb9f8c346a94530e77143462d05ca5d65
rramonfraga <rafael_ramon_fraga@yahoo.es>
2017-01-08 23:18:58 +0100

    Create direct second half

--- app/models/championship.rb
+++ app/models/championship.rb
@@ -7,17 +7,12 @@ class Championship < ApplicationRecord
 
   validates :name, :kind, :community_id, presence: true
 
-  def create_seasons
-    (@teams.count - 1).times do |index|
-      season = seasons.create round: index + 1
-      season.create_matches(@teams)
-      rotate_teams
-    end
+  def direct_first_half
+    create_seasons(1, true)
   end
 
-  def rotate_teams
-    last = @teams.pop
-    @teams.insert(1, last)
+  def direct_second_half
+    create_seasons(number_of_teams, false)
   end
 
   def clasification
@@ -33,12 +28,34 @@ class Championship < ApplicationRecord
 
   def start!
     self.start = true
-    @teams = teams.to_a
-    create_seasons
+    direct_first_half
     save!
   end
 
   def community_code
     community_id
+  end
+
+  private
+
+  def create_seasons(round, first_half)
+    (number_of_teams - 1).times do |index|
+      season = seasons.create round: index + round
+      season.create_matches(ordered_teams, first_half)
+      rotate_teams
+    end
+  end
+
+  def rotate_teams
+    last = ordered_teams.pop
+    ordered_teams.insert(1, last)
+  end
+
+  def ordered_teams
+    @_ordered_teams ||= teams.sort_by(&:id)
+  end
+
+  def number_of_teams
+    @_number_of_teams ||= ordered_teams.count
   end
 end
--- app/models/season.rb
+++ app/models/season.rb
@@ -2,16 +2,25 @@ class Season < ApplicationRecord
   belongs_to :championship
   has_many :matches
 
-  def create_matches(teams)
-    rounds = teams.size/2
-    rounds.times do |index|
-      matches.create  host_team: find_team(teams[index].id ),
-                      visit_team: find_team(teams[ (teams.size - 1) - index].id )
+  def create_matches(teams, first_half)
+    @teams = teams
+    (number_of_teams/2).times do |index|
+      matches.create host_team: set_host_team(index, first_half),
+                     visit_team: set_visit_team(index, first_half)
     end
   end
 
   private
-  def find_team(id)
-    Team.find_by(id: id)
+
+  def set_host_team(index, first_half)
+    first_half.present? ? @teams[index] : @teams[(number_of_teams - 1) - index]
+  end
+
+  def set_visit_team(index, first_half)
+    first_half.present? ? @teams[(number_of_teams - 1) - index] : @teams[index]
+  end
+
+  def number_of_teams
+    @_number_of_teams ||= @teams.count
   end
 end
