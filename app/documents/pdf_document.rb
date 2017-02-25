a10ced8c3385394f0bdb143a0982b3770269a50f
rramonfraga <rafael_ramon_fraga@yahoo.es>
2017-02-25 18:09:00 +0100

    Add new injury icons

--- app/documents/pdf_document.rb
+++ app/documents/pdf_document.rb
@@ -7,7 +7,7 @@ class PdfDocument < Prawn::Document
 
   def initialize(team, options = {})
     @team = team
-    @players = team.live_players
+    @players = team.live_players.order('player_template_id ASC')
     self.options = options.reverse_merge(page_size: 'A4', page_layout: :landscape)
     super(self.options)
   end
@@ -29,7 +29,7 @@ class PdfDocument < Prawn::Document
 
   def header
     text "#{@team.name} - #{@team.team.name} - #{@team.user.name}", size: 20, style: :bold
-    image Rails.root.join('public/images/logo-spiked-ball.png'), width: 100, at: [bounds.right - 113, bounds.top - 0]
+    image Rails.root.join('public/images/logo-spiked-ball.png'), width: 95, at: [bounds.right - 95, bounds.top - 0]
 
     font_size 14
     text "Treasury: #{@team.treasury} - Value: #{@team.value}"
@@ -42,7 +42,7 @@ class PdfDocument < Prawn::Document
     16.times.each do |index|
       data << [bold(index + 1)] + add_players(@players[index])
     end
-    table(data, width: 770, cell_style: { inline_format: true }, column_widths: column_widths) do |table|
+    table(data, width: 770, cell_style: { inline_format: true, position: :center }, column_widths: column_widths) do |table|
       table.row(0).background_color = "65C5E4"
       table.column(0).background_color = "65C5E4"
       table.row(0).text_color = "ECF0F1"
@@ -51,10 +51,9 @@ class PdfDocument < Prawn::Document
   end
 
   def footer
-    table(extras, cell_style: { inline_format: true }, column_widths: [130, 50]) do |table|
-      table.column(1..-1).each{|c| c.background_color = "65C5E4" }
-      table.column(1..-1).each{|c| c.background_color = "ECF0F1" }
-    end
+    table(extras,
+          cell_style: { inline_format: true, background_color: "65C5E4", text_color: "ECF0F1" },
+          column_widths: [100, 30, 100, 30, 100, 30])
   end
 
   def player_head
@@ -70,7 +69,7 @@ class PdfDocument < Prawn::Document
   def injured_player(player)
     [
       strike(player.dorsal),
-      strike(player.name),
+      make_table([[{ image: 'app/assets/images/injury.png', image_width: 12 }, content: strike(player.name)]], cell_style: { inline_format: true, borders: [] }),
       strike(player.title),
       strike(player.ma),
       strike(player.st),
@@ -78,8 +77,8 @@ class PdfDocument < Prawn::Document
       strike(player.av),
       strike(player.cost),
       strike(player.full_skills),
-      strike(player.niggling_injury),
-      strike(player.experience)
+      niggling_injury(player.niggling_injury),
+      strike(experience(player.experience))
     ]
   end
 
@@ -94,23 +93,35 @@ class PdfDocument < Prawn::Document
       player.av,
       player.cost,
       player.full_skills,
-      player.niggling_injury,
-      player.experience
+      niggling_injury(player.niggling_injury),
+      experience(player.experience)
     ]
   end
 
+  def niggling_injury(niggling_injury)
+    return "" if niggling_injury.zero?
+    return niggling_injury_image if niggling_injury == 1
+    make_table([ [niggling_injury_image, "x #{niggling_injury}"] ], width: 40, column_widths: [15,25], cell_style: { borders: [] })
+  end
+
+  def experience(experience)
+    return "" if experience.zero?
+    experience
+  end
+
   def column_widths
-    [30, 50, 110, 90, 40, 40, 40, 40, 70, 160, 50, 50]
+    [30, 40, 110, 90, 27, 27, 27, 27, 47, 275, 40, 30]
   end
 
   def extras
     [
-      ["Re-Rolls", @team.re_rolls],
-      ["Fan Factor", @team.fan_factor],
-      ["Assistant Coaches", @team.assistant_coaches],
-      [ "Cheerleaders", @team.cheerleaders],
-      ["Apothecaries", @team.apothecaries]
+      ["Re-Rolls", @team.re_rolls, "Assistant Coaches", @team.assistant_coaches, "Apothecaries", @team.apothecaries],
+      ["Fan Factor", @team.fan_factor, "Cheerleaders", @team.cheerleaders]
     ]
+  end
+
+  def niggling_injury_image
+    { image: 'app/assets/images/niggly_injury.png', image_width: 12 }
   end
 
   def strike(value)
